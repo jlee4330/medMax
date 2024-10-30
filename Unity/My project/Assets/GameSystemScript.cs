d12d152e5d7595eccdd602f8ad60de4a9a2ffd9d
PSuHyeon <0111suhyeon@kaist.ac.kr>
2024-10-31 03:42:53 +0900

    Unity Player and Location & Node.js location server

---
+++ Unity/My project/Assets/GameSystemScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSystemScript : MonoBehaviour
+{
+    public static List<GameObject> player_arr;
+    public int num_of_players;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // player_arr = new List<GameObject>(); 
+        // num_of_players = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
