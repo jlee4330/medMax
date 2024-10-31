ee295a411f930e7d6b6b04288a93c37e75173661
PSuHyeon <0111suhyeon@kaist.ac.kr>
2024-10-31 13:05:07 +0900

    Broadcast Position to Other Client

--- backEnd/Websocket.js
+++ backEnd/Websocket.js
@@ -8,8 +8,13 @@ wss.on('connection', function connection(ws) {
       const x = message[0];
       const y = message[1];
       console.log('데이터 : \n x = %o, y = %o', x,y);
-      ws.send(data);
+      wss.clients.forEach(client => {
+         if (client.readyState === WebSocket.OPEN) {
+             client.send(data);
+         }
+     });
    })
+   
 })
 wss.on('listening',()=>{
    console.log('listening on 7777')
