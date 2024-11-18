6e9acefe7065b75af1a8e6ee7a1f8c4617d3bcc2
Shin Seowon <sswilove1@kaist.ac.kr>
2024-11-19 03:10:17 +0900

    Q list components update & modal design
    question -> q, content, pharmacist, answer
    tomorrow : develop modal state 2,3

--- frontEnd/src/tabs/qna.tsx
+++ frontEnd/src/tabs/qna.tsx
@@ -5,26 +5,36 @@ import { TabView, SceneMap } from 'react-native-tab-view';
 //두 종류를 옆으로 넘기기 위해서 tab view를 쓸 수 있대 -> 근데 FE에 tab view랑 pager view 설치해야 함 !!
 
 const MyQList = [
-  { id: '1', question: '체질에 맞는 약이 뭔가요?' },
-  //일단은 모든 질문에 대해서 답변 내용 동일하게 나오게 했는데 이걸 title, question, pharmacist, answer로 list 만들어서 토글했을 때 보이는 내용 바꿔야겠다
-  { id: '2', question: '약이 너무 독한건가요?' },
-  { id: '3', question: '저한테 맞는 복용 방법은?' },
+  { id: '1', question: '체질에 맞는 약이 뭔가요?', content: '체질에 맞는 약이 뭐냐구우', pharmacist: '신서원 약사', answer: '오키바리' },
+  { id: '2', question: '약이 너무 독한건가요?', content: '약이 너무 독하냐구우', pharmacist: '이한샘 약사', answer: '옛설' },
+  { id: '3', question: '저한테 맞는 복용 방법은?', content: '나에게 맞는 복용방법이 뭐냐구우', pharmacist: '안영은 약사', answer: '흠 그것은 말이죠' },
 ];
 
 const FAQList = [
-  { id: '4', question: '이 약, 저한테 부작용 없을까요?' },
-  { id: '5', question: '저처럼 바쁜 사람에게 약 시간 관리법은?' },
+  { id: '4', question: '이 약, 저한테 부작용 없을까요?', content: '부작용이 있을까요오', pharmacist: '이동건 약사', answer: '그럴수도 아닐수도' },
+  { id: '5', question: '저처럼 바쁜 사람에게 약 시간 관리법은?', content: '약 시간 관리법 알려주세요오', pharmacist: '이주영 약사', answer: '제때 드세욥!' },
 ];
 
-const AccordionItem = ({ item }) => { //토글로 열고 닫는 걸 accordion이라고
+const SimilarQList = [
+  { id: '6', question: '가장 유사한 질문1', content: '작성하신 질문과 비슷한 질문은~1', pharmacist: '박수현 약사', answer: 'AI 파이팅' },
+  { id: '7', question: '가장 유사한 질문2', content: '작성하신 질문과 비슷한 질문은~2', pharmacist: '정예준 약사', answer: '백 연결 할 수 있겠지' },
+  { id: '8', question: '가장 유사한 질문3', content: '작성하신 질문과 비슷한 질문은~3', pharmacist: '카카오 약사', answer: '물론이지 ㅋㅋ' },
+];
+
+const AccordionItem = ({ item }) => { //토글로 열고 닫는 걸 accordion이라고 칭하자
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <TouchableOpacity onPress={() => setIsOpen(!isOpen)} style={styles.itemContainer}>
       <Text style={styles.itemTitle}>{item.question}</Text>
-      {isOpen && <Text style={styles.itemContent}>여기에 질문에 대한 답변 내용 넣기 !!</Text>}
+      {isOpen && (
+      <>
+        <Text style={styles.itemContent}>{item.content}</Text>
+        <Text style={styles.itemPharmacist}>{item.pharmacist}</Text>
+        <Text style={styles.itemAnswer}>{item.answer}</Text>
+      </>)}
     </TouchableOpacity>
-  ); //question, content -> title, question, pharmacist, answer로 수정하고 각각의 디자인 추가하기
+  ); //question, content, pharmacist, answer 각각의 디자인 추가하기
 };
 
 const MyQA = ({ searchQuery }) => {
@@ -59,6 +69,7 @@ const App = () => {
   const [searchQuery, setSearchQuery] = useState('');
   const [isModalVisible, setIsModalVisible] = useState(false);
   const [modalText, setModalText] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState('');
 
   const renderScene = SceneMap({
     myQA: () => <MyQA searchQuery={searchQuery} />,
@@ -102,15 +113,43 @@ const App = () => {
           <View style={styles.modalContainer}>
             <View style={styles.modalContent}>
               <Text style={styles.modalTitle}>Q&A 작성하기</Text>
+              <View style={styles.categoryContainer}>
+                <TouchableOpacity
+                  style={[styles.categoryButton, selectedCategory === '복용' && styles.categoryButtonSelected]}
+                  onPress={() => setSelectedCategory('복용')}
+                >
+                  <Text style={styles.categoryText}>복용</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={[styles.categoryButton, selectedCategory === '건강 관리' && styles.categoryButtonSelected]}
+                  onPress={() => setSelectedCategory('건강 관리')}
+                >
+                  <Text style={styles.categoryText}>건강 관리</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={[styles.categoryButton, selectedCategory === '부작용' && styles.categoryButtonSelected]}
+                  onPress={() => setSelectedCategory('부작용')}
+                >
+                  <Text style={styles.categoryText}>부작용</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                  style={[styles.categoryButton, selectedCategory === '기타' && styles.categoryButtonSelected]}
+                  onPress={() => setSelectedCategory('기타')}
+                >
+                  <Text style={styles.categoryText}>기타</Text>
+                </TouchableOpacity>
+              </View>
               <TextInput
-                style={styles.modalInput}
-                placeholder="여기에 내용을 입력하세요"
+                style={styles.textArea}
+                placeholder="안녕하세요 약사님, 최근에 OOO 약을 처방받았는데..."
                 value={modalText}
                 onChangeText={setModalText}
+                multiline={true}
               />
               <View style={styles.modalButtons}>
                 <Button title="취소" onPress={() => setIsModalVisible(false)} />
-                <Button title="다음으로" onPress={() => setIsModalVisible(false)} />
+                <Button title="다음으로" onPress={() => setIsModalVisible(false)} /> 
+                {/* onPress={handleNextStep} */}
               </View>
             </View>
           </View>
@@ -160,6 +199,15 @@ const styles = StyleSheet.create({
       fontSize: 14,
       color: '#555',
     },
+    itemPharmacist: {
+      marginTop: 20,
+      fontSize: 15,
+    },
+    itemAnswer: {
+      marginTop: 10,
+      fontSize: 14,
+      color: '#555',
+    },
     floatingButton: {
       position: 'absolute',
       bottom: 20,
@@ -198,13 +246,33 @@ const styles = StyleSheet.create({
       fontWeight: 'bold',
       marginBottom: 15,
     },
-    modalInput: {
-      width: '100%',
-      height: 40,
+    categoryContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      marginBottom: 15,
+    },
+    categoryButton: {
+      padding: 8,
       borderWidth: 1,
-      borderColor: '#ddd',
+      borderColor: 'lightgray',
+      borderRadius: 8,
+      marginHorizontal: 5,
+    },
+    categoryButtonSelected: {
+      backgroundColor: 'purple',
+      borderColor: 'purple',
+    },
+    categoryText: {
+      color: 'black',
+    },
+    textArea: {
+      width: '100%',
+      height: 300,
+      borderColor: 'lightgray',
+      borderWidth: 1,
       borderRadius: 5,
       padding: 10,
+      textAlignVertical: 'top', // 여러 줄 입력 시 텍스트 위쪽 정렬
       marginBottom: 15,
     },
     modalButtons: {
