7ab7f5fb95a6c98ba9916991f90db6b19844e084
damhs <ihansaem1@kaist.ac.kr>
2024-11-22 00:10:04 +0900

    Add myPage API

--- backEnd/src/Routers/myPageRouter.js
+++ backEnd/src/Routers/myPageRouter.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getCalender } = require("../Services/myPageService");
+const { getCalender, getThirtyDay, getSevenDay, getTogetherMedi, getFullMedi } = require("../Services/myPageService");
 
 const myPageRouter = express.Router();
 
@@ -25,5 +25,37 @@ myPageRouter.get("/thirty-day", async (req, res) => {
     }
 });
 
+// 7일 복약 조회 API
+myPageRouter.get("/seven-day", async (req, res) => {
+    const { userId } = req.query;
+    try {
+        const sevenDay = await getSevenDay(userId);
+        res.status(200).json(sevenDay);
+    } catch (error) {
+        res.status(500).json({ error: "Failed to fetch SevenDayMedi" });
+    }
+});
+
+// 함께 복약 조회 API
+myPageRouter.get("/together-medi", async (req, res) => {
+    const { userId } = req.query;
+    try {
+        const togetherMedi = await getTogetherMedi(userId);
+        res.status(200).json(togetherMedi);
+    } catch (error) {
+        res.status(500).json({ error: "Failed to fetch TogetherMedi" });
+    }
+});
+
+// 모두 복약 조회 API
+myPageRouter.get("/full-medi", async (req, res) => {
+    const { userId } = req.query;
+    try {
+        const fullMedi = await getFullMedi(userId);
+        res.status(200).json(fullMedi);
+    } catch (error) {
+        res.status(500).json({ error: "Failed to fetch FullMedi" });
+    }
+});
 
 module.exports = myPageRouter;
--- backEnd/src/Services/myPageService.js
+++ backEnd/src/Services/myPageService.js
@@ -1,46 +1,75 @@
+const { get } = require("http");
 const pool = require("../mysql.js");
 
 const getCalender = async (userId) => { // 지난주 일요일부터 오늘까지의 복약 횟수를 가져오는 함수
     try {
         const [result] = await pool.query(
-            "SELECT date, mediCount FROM Date_medi WHERE user_id = ? AND date >= DATE_SUB(CURDATE(), INTERVAL 7 DAY)",
+            "SELECT DATE_FORMAT(mediDate, '%Y-%m-%d') AS mediDate, mediCount FROM Date_medi WHERE userId = ? AND mediDate >= DATE_SUB(CURDATE(), INTERVAL WEEKDAY(CURDATE()) + 8 DAY) AND mediDate <= CURDATE();",
             [userId]
         );
-        return result.map(row => [row.date, row.count]);
+        return result.map(row => [row.mediDate, row.mediCount]);
     } catch(error) {
         console.error("Error fetching calender:", error);
         throw error;
     }
 };
 
-const getThirtyDay = async (userID) => {
+const getThirtyDay = async (userId) => {
     try {
         const [result] = await pool.query(
-            "SELECT mediCount, Count(date) AS dateCount FROM Date_medi WHERE user_id = ? AND date >= DATE_SUB(CURDATE(), INTERVAL 30 DAY) GROUP BY mediCount ORDER BY mediCount DESC",
+            "SELECT mediCount, Count(mediDate) AS mediDateCount FROM Date_medi WHERE userId = ? AND mediDate >= DATE_SUB(CURDATE(), INTERVAL 30 DAY) AND mediDate <= CURDATE() GROUP BY mediCount ORDER BY mediCount DESC",
             [userId]
         );
-        return result.map(row => [row.dateCount, row.mediCount]);
+        return result.map(row => [row.mediCount, row.mediDateCount]);
     } catch(error) {
         console.error("Error fetching ThirtyDayMedi:", error);
         throw error;
     }
 };
 
-const getSevenDay = async (userID) => {
+const getSevenDay = async (userId) => {
     try {
         const [result] = await pool.query(
-            "SELECT mediCount, Count(date) AS dateCount FROM Date_medi WHERE user_id = ? AND date >= DATE_SUB(CURDATE(), INTERVAL 7 DAY) GROUP BY mediCount ORDER BY mediCount DESC",
+            "SELECT mediCount, Count(mediDate) AS mediDateCount FROM Date_medi WHERE userId = ? AND mediDate >= DATE_SUB(CURDATE(), INTERVAL 7 DAY) AND mediDate <= CURDATE() GROUP BY mediCount ORDER BY mediCount DESC",
             [userId]
         );
-        return result.map(row => [row.dateCount, row.mediCount]);
+        return result.map(row => [row.mediCount, row.mediDateCount]);
     } catch(error) {
         console.error("Error fetching SevenDayMedi:", error);
         throw error;
     }
 };
 
+const getTogetherMedi = async (userId) => {
+    try {
+        const [result] = await pool.query(
+            "SELECT COUNT(mediDate) AS mediDateCount FROM Date_medi WHERE userId = ? AND mediCount >= 1",
+            [userId]
+        );
+        return result.map(row => [row.mediDateCount]);
+    } catch(error) {
+        console.error("Error fetching TogetherMedi:", error);
+        throw error;
+    }
+}
+
+const getFullMedi = async (userId) => {
+    try {
+        const [result] = await pool.query(
+            "SELECT Count(mediDate) AS mediDateCount FROM Date_medi WHERE userId = ? AND mediCount = (SELECT numMedi FROM User WHERE userId = ?)",
+            [userId, userId]
+        );
+        return result.map(row => [row.mediDateCount]);
+    } catch(error) {
+        console.error("Error fetching FullMedi:", error);
+        throw error;
+    }
+}
+
 module.exports = {
     getCalender,
     getThirtyDay,
     getSevenDay,
+    getTogetherMedi,
+    getFullMedi
 };
